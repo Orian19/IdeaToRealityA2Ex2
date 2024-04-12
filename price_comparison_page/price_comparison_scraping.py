0cd0e7ea7558a5d487f026ca22abee18714054e0
Orian19 <orian19@gmail.com>
2024-04-12 22:34:42 +0300

    [IdeaToReality][2] use undetected chrome_driver instead
    solves issue with annoying walmart captcha

--- price_comparison_page/price_comparison_scraping.py
+++ price_comparison_page/price_comparison_scraping.py
@@ -1,17 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+import undetected_chromedriver as uc
 import pandas as pd
 import time
 
 
 def get_webdriver():
-    options = webdriver.ChromeOptions()
+    options = uc.ChromeOptions()
+    # options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver = uc.Chrome(options=options)
+
+    return driver
 
 
 def scrape_site(driver, url, site_name):
